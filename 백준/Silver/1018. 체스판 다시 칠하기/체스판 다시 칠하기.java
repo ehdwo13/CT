61beff2079ea586045dcea570fa98b26be4e4a0e
박동재 <156689148+ehdwo13@users.noreply.github.com>
2025-01-10 20:32:28 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 208 ms, Memory: 19344 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        sc.nextLine();
+        char[][] board = new char[N][M];
+
+        for(int i=0; i<N; i++){
+            String line = sc.nextLine();
+            for(int j=0; j<M; j++){
+                board[i][j] = line.charAt(j);
+            }
+        }
+
+        int count = Integer.MAX_VALUE;
+
+        for(int i=0; i<=N-8; i++){
+            for(int j=0; j<=M-8; j++){
+                count = Math.min(count, getCount(board, i, j));
+            }
+        }
+        System.out.println(count);
+    }
+    private static int getCount(char[][] board, int startX, int startY){
+        char[][] pattern1 = {
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'}
+        };
+
+        char[][] pattern2 = {
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'}
+        };
+        int count1 = 0;
+        int count2 = 0;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[startX + i][startY + j] != pattern1[i][j]) {
+                    count1++;
+                }
+                if (board[startX + i][startY + j] != pattern2[i][j]) {
+                    count2++;
+                }
+            }
+        }
+        return Math.min(count1, count2);
+    }
+}
