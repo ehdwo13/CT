5509bc9af7f64ce4208d85147028662f8284da5d
박동재 <156689148+ehdwo13@users.noreply.github.com>
2025-01-14 05:12:22 +0900

    [Silver IV] Title: 대칭 차집합, Time: 744 ms, Memory: 89728 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        HashSet<Integer> A = new HashSet<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A.add(Integer.parseInt(st.nextToken()));
+        }
+        HashSet<Integer> B = new HashSet<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            B.add(Integer.parseInt(st.nextToken()));
+        }
+        HashSet<Integer> intersection = new HashSet<>(A);
+        intersection.retainAll(B);
+        int size = (A.size() + B.size()) - 2 * intersection.size();
+        System.out.println(size);
+    }
+}
