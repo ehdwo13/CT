8e984b481c85d4f3828696144d066dcb856d5d6b
박동재 <156689148+ehdwo13@users.noreply.github.com>
2025-01-16 20:20:37 +0900

    [Silver IV] Title: 괄호, Time: 192 ms, Memory: 18052 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            String input = sc.nextLine();
+            System.out.println(isVPS(input) ? "YES" : "NO");
+        }
+    }
+
+    public static boolean isVPS(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
