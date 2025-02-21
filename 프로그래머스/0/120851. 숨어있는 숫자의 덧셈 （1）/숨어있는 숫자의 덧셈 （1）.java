782b21936e7ae393c8ee6dfc94d2f833e5025494
박동재 <156689148+ehdwo13@users.noreply.github.com>
2025-02-21 13:11:48 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 2.76 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int solution(String my_string) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for(char ch : my_string.toCharArray()){
+            if(Character.isDigit(ch)){
+                arrayList.add(ch - '0');
+            }
+        }
+        return arrayList.stream().mapToInt(i -> i).sum();
+    }
+}
