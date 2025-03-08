d40660245d2ad19d4436814caaa5c3def442f383
박동재 <156689148+ehdwo13@users.noreply.github.com>
2025-03-08 20:35:15 +0900

    [level 0] Title: l로 만들기, Time: 11.08 ms, Memory: 90.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String myString) {
+        StringBuilder result = new StringBuilder();
+        for (char ch : myString.toCharArray()) {
+            if (ch < 'l') {
+                result.append('l');
+            } else {
+                result.append(ch);
+            }
+        }
+        return result.toString();
+    }
+}
