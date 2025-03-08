97ffb17f3a9e465cd46f1f0293917143289c1813
박동재 <156689148+ehdwo13@users.noreply.github.com>
2025-03-08 20:39:04 +0900

    [level 0] Title: 커피 심부름, Time: 0.97 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String[] order) {
+        int total = 0;
+
+        for (String menu : order) {
+            if (menu.contains("cafelatte")) {
+                total += 5000;
+            } else {
+                total += 4500;
+            }
+        }
+
+        return total;
+    }
+}
